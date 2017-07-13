ad7d6dc3fa857be41bfef4cda3464a6c6c4bb7fa
Lucas Merencia <lucas.merencia@gmail.com>
2017-07-13 20:44:04 -0300

    glide

--- server/server.go
+++ server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"net/http"
 
+	"github.com/apex/log"
 	"github.com/gorilla/mux"
 	"github.com/merencia/gorreios/config"
 	"github.com/merencia/gorreios/controllers"
@@ -13,9 +14,12 @@ func New() *http.Server {
 	mux := mux.NewRouter()
 
 	mux.HandleFunc("/track/{code}", controllers.TrackPackage)
+	conf := config.Get()
+
+	log.Info("starting on port " + conf.Port)
 
 	return &http.Server{
 		Handler: mux,
-		Addr:    ":" + config.Get().Port,
+		Addr:    ":" + conf.Port,
 	}
 }
